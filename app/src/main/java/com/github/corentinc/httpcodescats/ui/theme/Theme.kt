a2910c18a2155ac1eb35f647812bf7efba921006
Corentin C <57543006+corentin-c@users.noreply.github.com>
2024-07-24 09:04:49 +0200

    new android project

--- app/src/main/java/com/github/corentinc/httpcodescats/ui/theme/Theme.kt
+++ app/src/main/java/com/github/corentinc/httpcodescats/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.github.corentinc.httpcodescats.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
