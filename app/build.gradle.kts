19d53986ec66901bde9b2418fce0efe76d0c8dcc
Corentin C <57543006+corentin-c@users.noreply.github.com>
2025-05-14 14:10:59 +0200

    release 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
 		applicationId = "com.github.corentinc.httpcodescats"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 5
-		versionName = "1.1.1"
+		versionCode = 6
+		versionName = "1.1.2"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
