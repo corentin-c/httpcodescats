46d08ea69934feb5ca35b236e4bc48a348e77da7
Corentin C <57543006+corentin-c@users.noreply.github.com>
2025-05-14 13:56:15 +0200

    update ksp and make unit test runnable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,9 @@ dependencies {
 
 	// Tests
 	testImplementation(libs.mockk)
+	testImplementation(platform(libs.junit.bom))
 	testImplementation(libs.junit.jupiter)
+	testRuntimeOnly(libs.junit.platform.launcher)
 	testImplementation(libs.assertJCore)
 	testImplementation(libs.kotlinx.coroutines.test)
 
