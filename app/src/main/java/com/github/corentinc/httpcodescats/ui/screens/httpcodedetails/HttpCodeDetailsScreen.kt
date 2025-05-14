12c50798dc678f108d4765898131487a81095192
Corentin C <57543006+corentin-c@users.noreply.github.com>
2025-05-14 13:00:02 +0200

    fix code details not being scrollable

--- app/src/main/java/com/github/corentinc/httpcodescats/ui/screens/httpcodedetails/HttpCodeDetailsScreen.kt
+++ app/src/main/java/com/github/corentinc/httpcodescats/ui/screens/httpcodedetails/HttpCodeDetailsScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -55,7 +57,8 @@ fun HttpCodeDetailsContent(
 	Column(
 		modifier = Modifier
 			.fillMaxSize()
-			.padding(4.dp),
+			.padding(4.dp)
+			.verticalScroll(rememberScrollState()),
 		verticalArrangement = Arrangement.SpaceEvenly,
 		horizontalAlignment = Alignment.CenterHorizontally
 	) {
